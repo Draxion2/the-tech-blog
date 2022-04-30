b1ac8344ee5b282c0072caa164527c0e34e57706
= <holdanmcmullen@gmail.com>
2022-04-30 02:09:22 -0600

    get a user/create a user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -15,9 +15,61 @@ router.get("/", (req, res) => {
 });
 
 // get a single user
+router.get("/:id", (req, res) => {
+    User.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: { exclude: ["password"] },
+        include: [
+            {
+                model: Post,
+                attributes: ["id", "title", "contents", "created_at"]
+            },
+            {
+                model: Comment,
+                attributes: ["id", "comment_text", "created_at"],
+                include: {
+                    model: Post,
+                    attributes: ["title"]
+                }
+            }
+        ]
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: "No user found with this id"});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 // create a user
+router.post("/", withAuth, (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        });
+    })
+});
 
 // update a user
 
+
 // delete a user
+
+module.exports = router;

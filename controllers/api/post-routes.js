64a5d40778963aeaab04d0faa399416613f69afd
= <holdanmcmullen@gmail.com>
2022-04-30 01:11:37 -0600

    get a post route

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -37,6 +37,44 @@ router.get("/", (req, res) => {
 });
 
 // get a single post
+router.get("/:id", (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            "id",
+            "title",
+            "contents",
+            "created_at"
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+                include: {
+                    model: User,
+                    attributes: ["username"]
+                }
+            },
+            {
+                model: User,
+                attributes: ["username"]
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: "No post found with this id"});
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 // create a post
 

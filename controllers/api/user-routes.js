eba15e41dbbe43abd0b6878e4eaa961810aaf1c4
= <holdanmcmullen@gmail.com>
2022-04-30 02:15:41 -0600

    login & update a user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -67,8 +67,57 @@ router.post("/", withAuth, (req, res) => {
     })
 });
 
-// update a user
+// login a user
+router.post("/login", (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: "No user with that email address!"});
+            return;
+        }
 
+        // verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: "Incorrect password!"});
+            return;
+        }
+
+        req.session.save(() => {
+            // declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: "You are now logged in!" });
+        });
+    });
+});
+
+// update a user
+router.put("/:id", withAuth, (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData[0]) {
+            res.status(404).json({ message: "No user found with this id"});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 // delete a user
 

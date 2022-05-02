acb1e41f593a544ab2730ed8c318e37b7f0296e2
= <holdanmcmullen@gmail.com>
2022-05-02 01:32:31 -0600

    added dashboard js

--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,11 @@ const router = require("express").Router();
 
 const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes");
-// dashboard routes
+const dashboardRoutes = require("./dashboard-routes");
 
 router.use("/api", apiRoutes);
 router.use("/", homeRoutes);
-// use dashboard routes
+router.use("/dashboard", dashboardRoutes);
 
 router.use((req, res) => {
     res.status(404).end();

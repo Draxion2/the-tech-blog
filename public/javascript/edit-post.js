9f64758bf21a44e46081617a760a5c54a85d7d6e
= <holdanmcmullen@gmail.com>
2022-05-03 02:57:19 -0600

    edit post form

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -5,7 +5,7 @@ async function editFormHandler(event) {
         window.location.toString().split("/").length - 1
     ];
     const title = document.querySelector("input[name='post-title']").value;
-    const contents = document.querySelector("input[name='post-contents']").value;
+    const contents = document.querySelector("textarea").value;
     const response = await fetch(`/api/posts/${id}`, {
         method: "PUT",
         body: JSON.stringify({
